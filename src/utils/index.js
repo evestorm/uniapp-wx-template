2f399b9dd9c80e9d8d594efcd25215414fc7c449
Lance <1572534835@qq.com>
2021-08-18 13:56:34 +0800

    feat: 新增「数字超大时-末尾添加相应的单位」工具函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -110,6 +110,35 @@ export const outOfNum = (val, maxNum) => {
 };
 
 /**
+ * @description 数字超大时-末尾添加相应的单位
+ * @example
+ *  console.log(tranNumber(1000,2)) // 1000
+ * console.log(tranNumber(26742238,2)) // 2674.22万
+ * console.log(tranNumber(1234787325,2)) // 12.34亿
+ */
+
+export const tranNumber = (num, point) => {
+  // 将数字转换为字符串,然后通过split方法用.分隔,取到第0个
+  let numStr = num.toString().split(".")[0];
+  if (numStr.length < 6) {
+    // 判断数字有多长,如果小于6,,表示10万以内的数字,让其直接显示
+    console.log(numStr);
+    return numStr;
+  } else if (numStr.length >= 6 && numStr.length <= 8) {
+    // 如果数字大于6位,小于8位,让其数字后面加单位万
+    let decimal = numStr.substring(numStr.length - 4, numStr.length - 4 + point);
+    console.log(decimal);
+    // 由千位,百位组成的一个数字
+    return parseFloat(parseInt(num / 10000) + "." + decimal) + "万";
+  } else if (numStr.length > 8) {
+    // 如果数字大于8位,让其数字后面加单位亿
+    let decimal = numStr.substring(numStr.length - 8, numStr.length - 8 + point);
+    console.log(decimal);
+    return parseFloat(parseInt(num / 100000000) + "." + decimal) + "亿";
+  }
+};
+
+/**
  * ! ---------------------- 字符串相关 ----------------------
  */
 
